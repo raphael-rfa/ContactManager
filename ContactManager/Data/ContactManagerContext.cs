a1ffb1e73b4ee4da168918a6e4f15b431a44096d
Raphael Francisco Alves <raphaelbatealves@gmail.com>
2022-12-15 18:17:11 -0300

    @raphael-rfa [Task:Application in the MVC pattern with CRUD features.]{15/12/2022-18:17}

---
+++ ContactManager/Data/ContactManagerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContactManager.Models;
+
+namespace ContactManager.Data
+{
+    public class ContactManagerContext : DbContext
+    {
+        public ContactManagerContext (DbContextOptions<ContactManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContactManager.Models.Contact> Contact { get; set; } = default!;
+    }
+}
